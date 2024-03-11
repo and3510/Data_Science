767792e69ed0733ef26b571ef14d038708d77c9f
anderson <and.dovale@gmail.com>
2024-03-11 13:45:21 -0300

    comandos novos

--- main.sql
+++ main.sql
@@ -1,1 +1,15 @@
-CREATE DATABASE opa;
+-- Active: 1710116419724@@127.0.0.1@3306@cadastro
+
+ALTER TABLE `lojas`
+MODIFY COLUMN `Status` BOOLEAN NOT NULL;
+
+SELECT * FROM cadastro.lojas;
+
+alter table lojas CHANGE COLUMN Ativo ativo BOOLEAN not null;
+
+ALTER TABLE lojas change COLUMN quan_lojas quant_lojas int(100) not null;
+
+alter TABLE lojas add column valor float not null after ativo; 
+
+alter table lojas add column data_init year not null after id;
+insert into lojas (id,nome,categoria,ativo,valor,quant_lojas) values (1,'Renner','Roupas',1,120.000,45);
